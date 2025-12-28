1f2d68ea41e6c20039ee1620dd635cb50df4eb08
Sandy2k3 <sandeshgc2003@gmail.com>
2025-12-28 13:40:24 +0530

    first commit

---
+++ src/main/java/com/connections/travelConnect/TravelConnectApplication.java
@@ -0,0 +1,13 @@
+package com.connections.travelConnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/connections/travelConnect/TravelConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.connections.travelConnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
